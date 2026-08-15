c3d67234853b4c7eb929b882b69a5e3fc7abd50f
avishekray2006-hue <avishekray2006@gmail.com>
2026-08-15 18:00:14 +0530

    Update Kids Learning project

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
 
         <Route path="/login" element={<Login />} />
 
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/Signup" element={<Signup />} />
 
         <Route path="/courses" element={<Courses />} />
 
